0b52cb2217706ee723898dfbf7657913bd9158f0
pshrimangle <pshrimangle40@gmail.com>
2022-10-14 08:48:31 +0530

    added logic to launch the browser

--- src/main/java/com/AgileCrmAutomation/BaseClass.java
+++ src/main/java/com/AgileCrmAutomation/BaseClass.java
@@ -13,32 +13,34 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseClass {
 
-		public static WebDriver driver=null;
-		
+	public static WebDriver driver = null;
 	
-		public static void click(By by, String msg){
-			WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
-			wait.until(ExpectedConditions.elementToBeClickable(by));
-			driver.findElement(by).click();
-			System.out.println(msg);
-			}
-		public static void WaitForVisibilityOfElement(WebElement element) {
-			WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
-			wait.until(ExpectedConditions.visibilityOf(element));
-		}
-		public static void fluentwait(final By by) {
-			Wait<WebDriver> wait=new FluentWait<WebDriver>(driver)
-					.withTimeout(Duration.ofSeconds(10))
-					.pollingEvery(Duration.ofSeconds(2))
-					.ignoring(Exception.class);
-			wait.until(new Function<WebDriver, WebElement>(){
-				public WebElement apply(WebDriver driver) {
-					System.out.println("Waiting for element to be available...?");
-					return driver.findElement(by);
-				}
-			});
-		}
-		
-	}	
-		
+	public void launchBrowser(String browser) {
+		//launch browser code
+	}
+	
 
+	public static void click(By by, String msg) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(by));
+		driver.findElement(by).click();
+		System.out.println(msg);
+	}
+
+	public static void WaitForVisibilityOfElement(WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
+
+	public static void fluentwait(final By by) {
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(10))
+				.pollingEvery(Duration.ofSeconds(2)).ignoring(Exception.class);
+		wait.until(new Function<WebDriver, WebElement>() {
+			public WebElement apply(WebDriver driver) {
+				System.out.println("Waiting for element to be available...?");
+				return driver.findElement(by);
+			}
+		});
+	}
+
+}
